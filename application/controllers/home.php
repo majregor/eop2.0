d8b5905ce7cfe8426cb1ad1babbc4307a8086133
Godfrey Majwega <majregor@gmail.com>
2015-05-28 17:28:10 -0400

    v1.0.1.9
    - Finalized School Selection by District Admin
    - Loading School object into session for school users and school admins.
    - Home Screen Steps 1,2 and 3 pages 90% complete
    - Added automatic user subscription to district according to selected
    school while adding school users and school admins.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -16,7 +16,10 @@ class Home extends CI_Controller{
             // Load the home screen if the user is logged in
             $this->template->set('page_title', 'Home');
             $this->template->set('step_title', 'Getting Started');
-            $this->template->load('template', 'home_screen');;
+            $data = array(
+                'step'  =>  1
+            );
+            $this->template->load('template', 'home_screen', $data);
         }
         else{
             // Redirect to login for if not logged in
@@ -24,5 +27,28 @@ class Home extends CI_Controller{
         }
 
 	}
+
+    /**
+     *  Action to load home steps pages
+     * @method step
+     * @param INT page number
+     */
+    public function step($step=1){
+        if($this->session->userdata('is_logged_in')){
+
+            $step = ($step<=3)? $step : 1;
+
+            $this->template->set('page_title', 'Home');
+            $this->template->set('step_title', 'Getting Started');
+            $data = array(
+                'step'  =>  $step
+            );
+            $this->template->load('template', 'home_screen', $data);
+        }
+        else{
+            // Redirect to login for if not logged in
+            redirect('/login');
+        }
+    }
 
 }
--- application/models/school_model.php
+++ application/models/school_model.php
@@ -23,6 +23,10 @@ class School_model extends CI_Model {
             $schoolData['district_id'] = null;
         }
 
+        if($this->session->userdata('user_id')){
+            $schoolData['owner'] = $this->session->userdata('user_id');
+        }
+
         $this->db->insert('eop_school', $schoolData);
         $school_id = $this->db->insert_id();
         $affected_rows = $this->db->affected_rows();
@@ -63,7 +67,7 @@ class School_model extends CI_Model {
     function getSchool($id){
 
         $condition = array('id' => $id);
-        $query = $this->db->get_where('eop_school', $condition);
+        $query = $this->db->get_where('eop_view_school', $condition);
 
         return $query->result_array();
     }
@@ -119,6 +123,7 @@ class School_model extends CI_Model {
      * Returns all schools available in a particular district
      *
      * @param string $district   The state to which the schools belong
+     * @return array results array of records
      */
     function getDistrictSchools($district){
         $conditions = array('district_id' => $district);
@@ -126,6 +131,62 @@ class School_model extends CI_Model {
         $query = $this->db->get_where('eop_view_school', $conditions);
 
         return $query->result_array();
+
+    }
+
+    /**
+     * Function getSchoolDistrict
+     * Returns a given school's district if any returns false if not
+     *
+     * @param string $school_id   The school id for whose district is needed
+     * @return int|null district id or null if none found
+     */
+    function getSchoolDistrict($school_id){
+        $returnVal = null;
+        $conditions = array('id' => $school_id);
+        $this->db->select('district_id');
+        $query = $this->db->get_where('eop_school', $conditions);
+
+        if($query->num_rows()>0){
+            $returnVal =  $query->result_array()[0]['district_id'];
+        }else{
+            $returnVal = null;
+        }
+
+        return $returnVal;
+    }
+
+
+    /**
+     *  Function attaches a school to the session object
+     * @method attach_to_session
+     * @param int school_id The school id
+     * @return void
+     */
+    public function attach_to_session($school_id){
+        //Clear loaded_school session data
+        $this->session->unset_userdata('loaded_school');
+
+        $schoolData = $this->getSchool($school_id);
+        $data = array(
+            'loaded_school' => array(
+                'id'                =>  $schoolData[0]['id'],
+                'district_id'       =>  $schoolData[0]['district_id'],
+                'state_val'         =>  $schoolData[0]['state_val'],
+                'name'              =>  $schoolData[0]['name'],
+                'screen_name'       =>  $schoolData[0]['screen_name'],
+                'description'       =>  $schoolData[0]['description'],
+                'created_date'      =>  $schoolData[0]['created_date'],
+                'modified_date'     =>  $schoolData[0]['modified_date'],
+                'owner'             =>  $schoolData[0]['owner'],
+                'state_permission'  =>  $schoolData[0]['state_permission'],
+                'district'          =>  $schoolData[0]['district'],
+                'state'             =>  $schoolData[0]['state']
+            )
+
+        );
+
+        $this->session->set_userdata($data);
 
     }
 
